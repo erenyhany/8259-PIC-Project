0f16d5429d43342493d5aa7ae0ce57054b455612
martinashraf01 <90205645+martinashraf01@users.noreply.github.com>
2023-12-21 22:46:47 +0200

    Update ControlLogic.v
    some changes for casc

--- ControlLogic.v
+++ ControlLogic.v
@@ -2,11 +2,12 @@ module ControlLogic(input A0, wire INTA,wire intreq,
   wire[4:1] ICWs, wire [4:1]OCWs,
  wire [7:0]datain ,output impulse1,impulse2,reg endOfImpulse2,reg INT ,
 reg AEOI,reg [4:0]beginOfvectorAdress ,reg [7:0]maskreg,
-reg[2:0]RR_RIS,reg [2:0]R_SL_EOI,reg OCW2isSent);
+reg[2:0]RR_RIS,reg [2:0]R_SL_EOI,reg OCW2isSent ,output reg endOfimp1,
+ reg SNGL,output reg[7:0]ICW3word );
 
 
     localparam icw1 = 1 , icw2 = 2 ,icw3=3 , icw4=4 ,ocw1=1,ocw2=2,ocw3=3;
-    reg[7:0] ICW1word,ICW2word,ICW3word,ICW4word;
+    reg[7:0] ICW1word,ICW2word,ICW4word;
     reg[7:0] OCW1word,OCW2word,OCW3word;
     always @(intreq)begin
         INT = intreq;
@@ -27,10 +28,17 @@ reg[2:0]RR_RIS,reg [2:0]R_SL_EOI,reg OCW2isSent);
         end
      end 
 
+    always@(posedge INTA )begin
+        if(impulse1 == 1 )begin
+        endOfimp1 = 1 ;
+         end 
+    end
      always @(posedge INTA)begin 
         if(counter ==2'b10)begin 
             endOfImpulse2=1;
+            endOfimp1 =0;
         end
+        
      end
 
     assign impulse1 = counter[1];
@@ -42,6 +50,7 @@ reg[2:0]RR_RIS,reg [2:0]R_SL_EOI,reg OCW2isSent);
         case(ICWs)
         4'b0001 :begin 
             ICW1word = datain;
+            SNGL = ICW1word[1];
             if(ICW1word[0] ==0)begin
                 AEOI = 0;
                 end
